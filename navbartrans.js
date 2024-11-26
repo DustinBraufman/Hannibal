4ded7e943f34e52492069e86150db3289efe45ad
DustinBraufman <djb226@lehigh.edu>
2024-11-25 21:48:25 -0500

    someshit

--- navbartrans.js
+++ navbartrans.js
@@ -1,15 +1,1 @@
-<script>
-    document.addEventListener("DOMContentLoaded", () => {
-        const navLinks = document.querySelectorAll(".nav a");
 
-        navLinks.forEach(link => {
-            link.addEventListener("click", () => {
-                // Remove 'active' class from all links
-                navLinks.forEach(link => link.classList.remove("active"));
-
-                // Add 'active' class to the clicked link
-                link.classList.add("active");
-            });
-        });
-    });
-</script>
